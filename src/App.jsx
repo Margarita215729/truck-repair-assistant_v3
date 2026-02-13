0e5e90e5e05d13acae4f6270f41860283de41a52
Margarita Makeeva <makeeva01.m@gmail.com>
2026-02-12 20:07:29 -0500

    Toaster update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from 'sonner'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
@@ -87,6 +88,7 @@ function App() {
             </Routes>
           </Router>
           <Toaster />
+          <SonnerToaster richColors position="top-right" />
         </QueryClientProvider>
       </AuthProvider>
     </LanguageProvider>
